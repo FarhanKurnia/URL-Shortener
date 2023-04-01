a728e10e484790592d9e40ff17796f97abbb4d87
farhankurnia <itsfarhankurnia@gmail.com>
2023-04-02 00:19:25 +0700

    add chain and remove welcome blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,16 @@
     <div class="container py-5">
       <div class="row">
         <div class="col-lg-6 mx-auto">
-          <h2 class="text-center font-weight-bold-center mb-4">Short your link</h3>
+          <h2 class="text-center font-weight-bold-center mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+              <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
+              <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
+            </svg>
+          Short your link
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-link" viewBox="0 0 16 16">
+            <path d="M6.354 5.5H4a3 3 0 0 0 0 6h3a3 3 0 0 0 2.83-4H9c-.086 0-.17.01-.25.031A2 2 0 0 1 7 10.5H4a2 2 0 1 1 0-4h1.535c.218-.376.495-.714.82-1z"/>
+            <path d="M9 5.5a3 3 0 0 0-2.83 4h1.098A2 2 0 0 1 9 6.5h3a2 2 0 1 1 0 4h-1.535a4.02 4.02 0 0 1-.82 1H12a3 3 0 1 0 0-6H9z"/>
+          </svg></h2>
           <p class="text-center">The easiest and simplest way to shorten your urls and make them more accessible</p>
           <div class="card shadow mb-5">
             <div class="card-body">
